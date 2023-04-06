9175d9120ce7cf79e89b1909be36e2ade7ee7726
Nils Bebelaar <nils@bebelaar.net>
2023-04-06 17:40:55 +0200

    Simplify config

--- config.py
+++ config.py
@@ -1,18 +1,10 @@
-from os import path
-from dotenv import load_dotenv
-from environs import Env
-
-basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, '.env'))
-env = Env()
-
 class Config:
     """Set Flask config variables."""
 
     FLASK_APP = 'start.py'
-    FLASK_DEBUG = env.bool('FLASK_DEBUG')
-    TESTING = env.bool('FLASK_TESTING')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///../' + env('SQLALCHEMY_DATABASE_PATH')
+    FLASK_DEBUG = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///../database/uitslag.sqlite'
 
     STATIC_FOLDER = 'static'
     TEMPLATES_FOLDER = 'templates'
